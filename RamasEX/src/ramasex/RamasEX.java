8cbf3246e72a986d9ff9043c66e118772985d1ea
Daniel Asénov <dasenovp01@educantabria.es>
2025-01-28 13:22:46 +0100

    Correción correcta

--- RamasEX/src/ramasex/RamasEX.java
+++ RamasEX/src/ramasex/RamasEX.java
@@ -5,7 +5,7 @@ public class RamasEX {
     public static void main(String[] args) {
         
         System.out.println("Mi proyecto de ED!");
-        System.out.println("c en la rama main!");
+        System.out.println("Correción en la rama main!");
         
     }
     
