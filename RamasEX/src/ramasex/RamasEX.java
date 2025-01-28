10c70ef66256679bc40ebcb81c750ed24292fa71
Daniel Asénov <dasenovp01@educantabria.es>
2025-01-28 13:20:19 +0100

    Código corregido

--- RamasEX/src/ramasex/RamasEX.java
+++ RamasEX/src/ramasex/RamasEX.java
@@ -5,7 +5,7 @@ public class RamasEX {
     public static void main(String[] args) {
         
         System.out.println("Mi proyecto de ED!");
-        System.out.println("Cambio en la rama main!");
+        System.out.println("Correción en la rama main!");
         
     }
     
